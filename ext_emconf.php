006b7bb7b49ff5fade23c3300ab0562dbe913433
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2022-10-17 10:21:51 +0200

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Powermail Captcha',
+    'description' => 'Captcha Extension for Powermail',
+    'category' => 'plugin',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.0',
+    'constraints' => [
+        'depends' => [
+            'powermail' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
