e3d44377eeb9588a739c46fa941d01a05c088d20
thomasrawiel <thomasrawiel@users.noreply.github.com>
2023-06-02 15:18:32 +0200

    fix formcontroller for powermail 8

--- Classes/Controller/FormController.php
+++ Classes/Controller/FormController.php
@@ -2,10 +2,7 @@
 
 namespace TRAW\Powermailcaptcha\Controller;
 
-use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\Exception\AspectNotFoundException;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\Exception;
 use TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotException;
 use TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException;
@@ -18,15 +15,12 @@ class FormController extends \In2code\Powermail\Controller\FormController
      * @throws InvalidSlotReturnException
      * @throws InvalidSlotException
      */
-    public function formAction(): ResponseInterface
+    public function formAction(): void
     {
         if ($this->settings['powermailcaptcha']['useSiteLanguage'] ?? 0) {
-            $currentLanguageId = GeneralUtility::makeInstance(Context::class)
-                ->getPropertyFromAspect('language', 'id');
-            $siteLanguage = $this->request->getAttribute('site')->getLanguageById($currentLanguageId);
-            $this->view->assign('languageIso', $siteLanguage->getTwoLetterIsoCode());
+            $this->view->assign('languageIso', $GLOBALS['TYPO3_REQUEST']->getAttribute('language')->getTwoLetterIsoCode());
         }
 
-        return parent::formAction();
+        parent::formAction();
     }
 }
