eadbdee146d23d59f4c0ee3200369abb9547108d
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-07-31 16:56:32 +0200

    Drop old version support

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'thomas.rawiel@gmail.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.6.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'powermail' => '9.0.0-12.99.99',
+            'typo3' => '12.4.0-12.4.99',
+            'powermail' => '12.0.0-12.99.99',
         ],
         'conflicts' => [
             'powermailrecaptcha' => '',
