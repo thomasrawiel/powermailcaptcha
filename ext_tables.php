e818e2f230249ea237665da0a06850c96e1de50e
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-07-31 16:56:02 +0200

    Migrate autoloaded page tsconfig

--- ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('TYPO3') or die('Access denied.');
-// Add Page TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:powermailcaptcha/Configuration/PageTsConfig/Powermailcaptcha.typoscript">'
-);
